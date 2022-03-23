cb627fb0efca32f6e0189cf3ab7931e02e014590
MajaQamp <98764757+MajaQamp@users.noreply.github.com>
2022-03-23 22:47:45 +0100

    Update ExtractPage.java

--- Maven/ExtractPage.java
+++ Maven/ExtractPage.java
@@ -10,22 +10,22 @@ import org.testng.annotations.Test;
 
 public class ExtractPage {
 
-    public static String URLtoCheck="https://www.placelab.com/extract";
+    public static String URLtoCheck = "https://www.placelab.com/extract";
     public String browser;
     public WebDriver driver;
 
-    public ExtractPage(){
+    public ExtractPage() {
 
     }
 
     @BeforeTest
-    public void openBrowser(){
-        this.browser="Firefox";
+    public void openBrowser() {
+        this.browser = "Firefox";
 
-        if (this.browser.contains("Chrome")){
+        if (this.browser.contains("Chrome")) {
             WebDriverManager.chromedriver().setup();
             this.driver = new ChromeDriver();
-        } else if (this.browser.contains("Firefox")){
+        } else if (this.browser.contains("Firefox")) {
             WebDriverManager.firefoxdriver().setup();
             this.driver = new FirefoxDriver();
         }
@@ -33,19 +33,25 @@ public class ExtractPage {
     }
 
     @Test
-    public void verifyLetsTalkRedButton(){
+    public void verifyLetsTalkRedButton() {
         String expectedText = "LET'S TALK";
         String actualText = "";
+        String otherOccurrence = "";
 
-        By by = new By.ByXPath("/html/body/header/div[1]/div/div/div/a/button/div");
+        By by = new By.ByCssSelector(".col-lg-6 > div:nth-child(1) > a:nth-child(3) > button:nth-child(1) > div:nth-child(2)");
+        By by2 = new By.ByXPath("//a[@class=\"nav-link lets-talk-header\"]");
+
+
         actualText = this.driver.findElement(by).getText();
+        otherOccurrence = this.driver.findElement(by2).getText();
 
         Assert.assertEquals(actualText, expectedText);
+        Assert.assertEquals(actualText, otherOccurrence);
 
     }
 
     @AfterTest
-    public void closeBrowser(){
+    public void closeBrowser() {
         this.driver.close();
     }
 
